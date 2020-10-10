b9f3bf5138553e76ac4aa44e77527bdd130d228e
BigBull21 <71898422+BigBull21@users.noreply.github.com>
2020-10-10 01:26:47 -0400

    implementing database

--- GUI.py
+++ GUI.py
@@ -53,22 +53,116 @@ def Search():
     itemname_search_entry.grid(row = 0, column = 1)
 
     lbl_search = Label(frame_search, text = 'Search by Query', font =('bold', 12), pady = 20)
-    lbl_search.grid(row = 1, column = 0, sticky=W)
+    lbl_search.grid(row = 1, column = 0, sticky = W)
     query_search = StringVar()
     query_search.set("Select * from contents where barcode>1")
     query_search.entry = Entry(frame_search, textvariable = query_search, width = 40)
     query_search.entry.grid(row = 1, column = 1)
 
+    
+    
+    frame_fields = Frame(search)
+    frame_fields.grid(row = 1, column = 0)
 
+    #name
+    itemname_text = StringVar()
+    itemname_label = Label(frame_fields, text = 'name', font = ('bold', 12))
+    itemname_label.grid(row = 0, column = 0, sticky = E)
+    itemname_entry = Entry(frame_fields, textvariable = itemname_text)
+    itemname_entry.grid(row = 0, column = 1, sticky = W)
+
+    #barcode
+    barcode_text = StringVar()
+    barcode_label = Label(frame_fields, text = 'barcode', font = ('bold', 12))
+    barcode_label.grid(row = 0, column = 2, sticky = E)
+    barcode_entry = Entry(frame_fields, textvariable = barcode_text)
+    barcode_entry.grid(row = 0, column = 3, sticky = W)
+
+
+    frame_contents = Frame(search)
+    frame_contents.grid(row = 2, column = 0, columnspan = 4, rowspan = 6, pady = 20, padx = 20)
+
+    columns = ['id', 'name', 'barcode']
+    contents_tree_view = Treeview(frame_contents, columns = columns, show = "headings")
+    contents_tree_view.column("id", width = 30)
+    
+    for col in columns[1:]:
+        contents_tree_view.column(col, width = 120)
+        contents_tree_view.heading(col, text = col)
+    
+    contents_tree_view.bind('<<TreeviewSelect>>', select_contents)
+    contents_tree_view.pack(side = "left", fill = "y")
+    scrollbar = Scrollbar(frame_contents, orient = 'vertical')
+    scrollbar.configure(command = contents_tree_view.yview)
+    scrollbar.pack(side = "right", fill = "y")
+    contents_tree_view.config(yscrollcommand = scrollbar.set)
+
+
+
+    frame_btns = Frame(search)
+    frame_btns.grid(row = 3, column = 0)
+
+    add_btn = Button(frame_btns, text = 'Add Content', width = 12, command = add_contents)
+    add_btn.grid(row = 0, column = 0, pady = 20)
+
+    remove_btn = Button(frame_btns, text = 'Remove Content', width = 12, command = remove_contents)
+    remove_btn.grid(row = 0, column = 1)
+
+    update_btn = Button(frame_btns, text = 'Update Content', width = 12, command = update_contents)
+    update_btn.grid(row = 0, column = 2)
+
+    clear_btn = Button(frame_btns, text = 'Clear Input', width = 12, command = clear_text)
+    clear_btn.grid(row = 0, column = 3)
+
+    search_btn = Button(frame_btns, text = 'Search', width = 12, command = search_itemname)
+    search_btn.grid(row = 0, column = 2)
+
+    search_query_btn = Button(frame_btns, text = 'Search Query', width = 12, command = execute_query)
+    search_query_btn.grid(row = 1, column = 2)
+
+
+
+    def populate_list(itemname = ''):
+       
+        for i in contents_tree_view.get_children():
+            contents_tree_view.delete(i)
+
+        for row in db.fetch(itemname):
+            contents_tree_view.insert('', 'end', values = row)
+
+    
+    def populate_list2(query = 'select * from contents'):
+        
+        for i in contents_tree_view.get_children():
+            contents_tree_view.delete(i)
+
+        for row in db.fetch2(query):
+            contents_tree_view.insert('', 'end', values = row)
+
+    
+    def add_contents():
+        
+        if itemname_text.get() == '' or barcode_text.get() == '':
+            messagebox.showerror('Required Fields', 'Please include all fields')
+            return
+
+        
+
+
+        
+
+
+
+    
 
 
 
 
 # Main window of GUI
-top = tkinter.Tk()
+top = Tk()
 top.geometry("200x200")
-tb = tkinter.Button(top, text = "Training!", command = Training, width=25, height=5, bg="Blue", fg="red")
-sb = tkinter.Button(top, text = "Search!", command = Search, width=25, height=5, bg="red", fg="blue")
+tb = Button(top, text = "Training!", command = Training, width=25, height=5, bg="Blue", fg="red")
+sb = Button(top, text = "Search!", command = Search, width=25, height=5, bg="red", fg="blue")
 tb.pack()
 sb.pack()
 top.mainloop()

a6aa1ba4e2ef7c06a72e450a1de4eec79a82baec
BigBull21 <71898422+BigBull21@users.noreply.github.com>
2020-10-11 01:27:01 -0400

    complete

--- GUI.py
+++ GUI.py
@@ -33,19 +33,8 @@ con = sql_connection()
  """
 # sql_table(con)
 
-def select_contents(event):
-        
-        try:
-            global selected_item
-            index = contents_tree_view.selection()[0]
-            selected_item = contents_tree_view.item(index)['values']
-            itemname_entry.delete(0, END)
-            itemname_entry.insert(END, selected_item[1])
-            barcode_entry.delete(0, END)
-            barcode_entry.insert(END, selected_item[2])
-        except IndexError:
-            pass
-        
+
+
 ## Function to pop up Training Window of GUI 
 def Training():
     top.destroy()
@@ -103,6 +92,20 @@ def Search():
     for col in columns[1:]:
         contents_tree_view.column(col, width = 120)
         contents_tree_view.heading(col, text = col)
+
+
+    def select_contents(event):
+        
+        try:
+            global selected_item
+            index = contents_tree_view.selection()[0]
+            selected_item = contents_tree_view.item(index)['values']
+            itemname_entry.delete(0, END)
+            itemname_entry.insert(END, selected_item[1])
+            barcode_entry.delete(0, END)
+            barcode_entry.insert(END, selected_item[2])
+        except IndexError:
+            pass
     
     contents_tree_view.bind('<<TreeviewSelect>>', select_contents)
     contents_tree_view.pack(side = "left", fill = "y")

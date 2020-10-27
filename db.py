fe29b502e73128effd05d7bc69e1497dcb6916a8
BigBull21 <71898422+BigBull21@users.noreply.github.com>
2020-10-27 16:05:53 -0400

    tuple fix

--- db.py
+++ db.py
@@ -1,4 +1,14 @@
 import sqlite3
+import ast
+
+def parse_tuple(string):
+    try:
+        s = ast.literal_eval(str(string))
+        if type(s) == tuple:
+            return s
+        return
+    except:
+        return
 
 class Database:
     
